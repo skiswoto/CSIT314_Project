592db19f72fed7ff9892bc526d61ba91b2705e27
destroybyBrian <brian913@live.com>
2025-11-07 16:20:09 +0800

    Create user admin & platform manager accounts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,187 +1,3 @@
-// import { userAuthStore } from '@/global/userAuthStore';
-// import { Tabs } from 'expo-router';
-// import { BarChart3, FileUser, User, UserRoundSearch } from 'lucide-react-native';
-
-
-// const TabLayout = (): React.ReactElement => {
-//     const user = userAuthStore((s) => s.user);
-//     const userRole = user?.user_metadata.role;
-
-//     const getAccessibleScreens = () => {
-//         const baseScreens = {
-//             Home: HomeScreen,
-//             Profile: ProfileScreen,
-//         }  
-
-//         const roleBasedScreens = {
-//             'pin': {},
-//             'csr_rep': {},
-//             'platform_manager': {},
-//             'user_admin': {},
-//         }
-//         return {...baseScreens, ...roleBasedScreens[user?.userRole]}
-//     }
-
-//     const screens = getAccessibleScreens()
-
-//     return (
-//         // <Tabs>
-//         //     <Tabs.Screen 
-//         //         name="home"
-//         //         options={{
-//         //             title:'Home',
-//         //             headerShown: false, 
-//         //             tabBarIcon: () => <UserRoundSearch />
-//         //         }}
-//         //     />
-//         //     <Tabs.Screen
-//         //         name="myListings"
-//         //         options={{
-//         //             title:'View Own Request (PIN)',
-//         //             headerShown: false, 
-//         //             tabBarIcon: () => <FileUser />
-//         //         }}
-//         //     />
-//         //     <Tabs.Screen
-//         //         name="savedRequest"
-//         //         options={{
-//         //             title:'Saved Requests',
-//         //             headerShown: false, 
-//         //             tabBarIcon: () => <FileUser />
-//         //         }}
-//         //     />
-//         //     <Tabs.Screen 
-//         //         name="analytics"
-//         //         options={{
-//         //             headerShown: false, 
-//         //             tabBarIcon: ({ color}) => <BarChart3 size={24} color={color} />,
-//         //             tabBarLabel: 'Analytics',
-//         //         }}
-//         //     />
-//         //     <Tabs.Screen 
-//         //         name="profile"
-//         //         options={{
-//         //             title:'Profile',
-//         //             headerShown: false, 
-//         //             tabBarIcon: () => <User />
-//         //         }}
-//         //     /> 
-//         // </Tabs>
-//         <Tabs>
-//             {Object.entries(screens).map(([name, component]) => (
-//                 <Tabs.Screen key={name} name={name} component={component} />
-//             ))}
-//         </Tabs>
-//     )
-// }
-
-// export default TabLayout;
-
-
-// import { userAuthStore } from '@/global/userAuthStore';
-// import { Tabs } from 'expo-router';
-// import { FileUser, User, UserRoundSearch } from 'lucide-react-native';
-// import Analytics from './analytics';
-// import Home from './home';
-// import MyListings from './myListings';
-// import Profile from './profile';
-// import SavedRequests from './savedRequest';
-
-
-// export default function TabLayout() {
-//     const user = userAuthStore((s) => s.user)
-//     // const userRole = user?.user_metadata?.role
-//     const userRole = user?.user_metadata?.role?.toLowerCase() as keyof typeof roleBasedTabs;
-
-//     const roleBasedTabs = {
-//         pin: [
-//             {
-//                 name: 'home',
-//                 component: Home,
-//                 options: { title: 'Home', headerShown: false, tabBarIcon: () => <UserRoundSearch /> },
-//             },
-//             {
-//                 name: 'myListings',
-//                 component: MyListings,
-//                 options: { title: 'View Own Request (PIN)', headerShown: false, tabBarIcon: () => <FileUser /> },
-//             },
-//             {
-//                 name: 'profile',
-//                 component: Profile,
-//                 options: { title: 'Profile', headerShown: false, tabBarIcon: () => <User /> },
-//             },
-//         ],
-//         csr_rep: [
-//             {
-//                 name: 'home',
-//                 options: { title: 'Home', headerShown: false, tabBarIcon: () => <UserRoundSearch /> },
-//             },
-//             {
-//                 name: 'savedRequest',
-//                 options: { title: 'Saved Requests', headerShown: false, tabBarIcon: () => <FileUser /> },
-//             },
-//             {
-//                 name: 'analytics',
-//                 options: { title: 'Analystics', headerShown: false,  },
-//             },
-//             {
-//                 name: 'profile',
-//                 options: { title: 'Profile', headerShown: false, tabBarIcon: () => <User /> },
-//             },
-//         ],
-//         platform_manager: [
-//             {
-//                 name: 'home',
-//                 options: { title: 'Home', headerShown: false, tabBarIcon: () => <UserRoundSearch /> },
-//             },
-//             {
-//                 name: 'profile',
-//                 options: { title: 'Profile', headerShown: false, tabBarIcon: () => <User /> },
-//             },
-//             {
-//                 name: 'analytics',
-//                 options: { title: 'Analystics', headerShown: false,  },
-//             },
-//             // platformAnalytics: PlatformAnalytics
-//         ],
-//         user_admin: [
-//             {
-//                 name: 'home',
-//                 component: Home,
-//                 options: { title: 'Home', headerShown: false, tabBarIcon: () => <UserRoundSearch /> },
-//             },
-//             {
-//                 name: 'profile',
-//                 component: Profile,
-//                 options: { title: 'Profile', headerShown: false, tabBarIcon: () => <User /> },
-//             },
-//             {
-//                 name: 'analytics',
-//                 component: Analytics,
-//                 options: { title: 'Analystics', headerShown: false,  },
-//             },
-//             // userActivity: UserActivity
-//         ],
-//     } as const
-
-//     const tabs = roleBasedTabs[userRole] ?? roleBasedTabs['pin']
-//     return (
-//         <Tabs>
-//             {!user ? (
-//                 <Tabs.Screen name="profile" options={{ headerShown: false }} />
-//             ) : (
-//                 tabs.map(tab => (
-//                     <Tabs.Screen
-//                         key={tab.name}
-//                         name={tab.name}
-//                         options={tab.options}
-//                     />
-//                 ))
-//             )}
-//         </Tabs>
-//     );
-// }
-
 import { userAuthStore } from '@/global/userAuthStore';
 import { Tabs } from 'expo-router';
 import { BarChart, FileUser, User, UserRoundSearch } from 'lucide-react-native';
@@ -189,9 +5,10 @@ import { BarChart, FileUser, User, UserRoundSearch } from 'lucide-react-native';
 
 export default function TabLayout() {
     const user = userAuthStore((s) => s.user);
-    const userRole = user?.user_metadata?.role?.toLowerCase() as 'pin' | 'csr_rep' | 'platform_manager' | 'user_admin';
+    const userRole = user?.user_metadata?.role?.toLowerCase() as 'unregistered' | 'pin' | 'csr_rep' | 'platform_manager' | 'user_admin';
 
     const roleBasedTabs = {
+        unregistered: ['home', 'profile'],
         pin: ['home', 'myListings', 'profile'],
         csr_rep: ['home', 'savedRequest', 'analytics', 'profile'],
         platform_manager: ['home', 'analytics', 'profile'],
@@ -236,8 +53,8 @@ export default function TabLayout() {
         },
     };
 
-    const visibleTabs = roleBasedTabs[userRole] ?? roleBasedTabs['pin'];
-    
+    const visibleTabs = roleBasedTabs[userRole] ?? roleBasedTabs['unregistered'];
+
     type TabName = keyof typeof tabConfig;
     const isTabVisible = (tabName: TabName): boolean => {
         return (visibleTabs as readonly TabName[]).includes(tabName);
