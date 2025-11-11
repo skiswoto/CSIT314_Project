5e64a2059b05b05241ab82af5f784a0beee633ee
destroybyBrian <brian913@live.com>
2025-11-11 20:54:47 +0800

    Fixed cookie and token bug

--- global/userAuthStore.tsx
+++ global/userAuthStore.tsx
@@ -19,46 +19,17 @@ export const userAuthStore = create<State & Action>((set) => ({
     clearUser: () => set({ user: null})
 }))
 
-const renewSessionIfExpired = async() => {
-    {
-        const { data, error } = await supabase.auth.getSession()
-        if (error) throw error
-        if (data?.session) {
-            await supabase.auth.setSession({
-                access_token: data.session.access_token,
-                refresh_token: data.session.refresh_token
-            })
-        }
-    }
-}
-
 // Initialize and sync with Supabase session
 supabase.auth.getUser().then(({ data: { user } }) => {
     userAuthStore.getState().setUser(user);
 });
 
 // Listen for auth changes (login/logout automatically update store)
-supabase.auth.onAuthStateChange(async(_event, session) => {
+const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
     userAuthStore.getState().setUser(session?.user ?? null);
 
-    if (session) {
-        await supabase.auth.setSession({
-            access_token: session.access_token,
-            refresh_token: session.refresh_token
-        })
-    }
 });
 
-renewSessionIfExpired()
-
-
-// // For when your session expired but cookies didnt update :
-// supabase.auth.getUser().then(async() => {
-//     userAuthStore.getState().clearUser()
-
-//     await supabase.auth.setSession({
-//         access_token: "",
-//         refresh_token: ""
-//     })
-// })
-
+export const unsubscribeAuth = () => {
+    authListener?.subscription.unsubscribe()
+}
